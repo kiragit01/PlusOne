9f9b09d4fc735478ef78b694931c0e19f7b479c8
kiragit01 <kiragithub@gmail.com>
2024-02-14 01:04:10 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PlusOne
+{
+    internal class Program
+    {
+        //https://leetcode.com/problems/plus-one/description/
+
+        static void Main(string[] args)
+        {
+            int[] digits = { 9,9,9,9 };
+            PlusOne(digits);
+            Console.ReadKey();
+        }
+
+
+        static int[] PlusOne(int[] digits)
+        {
+            int n = digits.Length;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                if (digits[i] == 9)
+                {
+                    if (i == 0)
+                    {
+                        int[] newdigits = new int[n + 1];
+                        newdigits[0] = 1;
+                        return newdigits;
+                    }
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
+            }
+            return digits;
+        }
+    }
+}
